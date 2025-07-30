02e5b2e6a9fa9608dc7d0573a60fa5ed26986025
Kushagra Tomar <kushagra.tomar@stackfusion.io>
2025-07-30 10:59:09 +0530

    initial commits

---
+++ main_replace.py
@@ -0,0 +1,244 @@
+import cv2
+import sys
+import argparse
+import numpy as np
+from PIL import Image
+from pathlib import Path
+from matplotlib import pyplot as plt
+from typing import Any, Dict, List
+
+import torch
+from diffusers import StableDiffusionInpaintPipeline
+from segment_anything import SamPredictor, sam_model_registry
+from utils.crop_for_replacing import recover_size, resize_and_pad
+
+def load_img_to_array(img_p):
+    img = Image.open(img_p)
+    # Checks if image has alpha channel
+    if img.mode == "RGBA":
+        img = img.convert("RGB")
+    # Converts image to a NumPy array and returns it
+    return np.array(img)
+
+def save_array_to_img(img_arr, img_p):
+    # Converts NumPy array to image and saves to path
+    Image.fromarray(img_arr.astype(np.uint8)).save(img_p)
+
+def dilate_mask(mask, dilate_factor=15):
+    mask = mask.astype(np.uint8) # Ensures mask is of uint8 type
+    mask = cv2.dilate(           # Applies morphological dilation
+        mask,
+        np.ones((dilate_factor, dilate_factor), np.uint8), # Kernel for dilation
+        iterations=1
+    )
+    return mask
+
+def show_mask(ax, mask: np.ndarray, random_color=False):
+    mask = mask.astype(np.uint8)
+    if np.max(mask) == 255:
+        mask = mask / 255
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_img = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_img)
+
+def show_points(ax, coords: List[List[float]], labels: List[int], size=375):
+    coords = np.array(coords)
+    labels = np.array(labels)
+    color_table = {0: 'red', 1: 'green'}
+    for label_value, color in color_table.items():
+        points = coords[labels == label_value]
+        ax.scatter(points[:, 0], points[:, 1], color=color, marker='*',
+                   s=size, edgecolor='white', linewidth=1.25)
+
+def get_clicked_point(img_path):
+    img = cv2.imread(img_path)
+    cv2.namedWindow("image")
+    cv2.imshow("image", img)
+
+    last_point = []
+    keep_looping = True
+
+    def mouse_callback(event, x, y, flags, param):
+        nonlocal last_point, keep_looping, img
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if last_point:
+                cv2.circle(img, tuple(last_point), 5, (0, 0, 0), -1)
+            last_point = [x, y]
+            cv2.circle(img, tuple(last_point), 5, (0, 0, 255), -1)
+            cv2.imshow("image", img)
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            keep_looping = False
+
+    cv2.setMouseCallback("image", mouse_callback)
+
+    while keep_looping:
+        cv2.waitKey(1)
+
+    cv2.destroyAllWindows()
+
+    return last_point
+
+def replace_img_with_sd(
+        img: np.ndarray,
+        mask: np.ndarray,
+        text_prompt: str,
+        step: int = 50,
+        device="cuda"
+):
+    pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-2-inpainting",
+        torch_dtype=torch.float32,
+    ).to(device)
+    img_padded, mask_padded, padding_factors = resize_and_pad(img, mask)
+    img_padded = pipe(
+        prompt=text_prompt,
+        image=Image.fromarray(img_padded),
+        mask_image=Image.fromarray(255 - mask_padded),
+        num_inference_steps=step,
+    ).images[0]
+    height, width, _ = img.shape
+    img_resized, mask_resized = recover_size(
+        np.array(img_padded), mask_padded, (height, width), padding_factors)
+    mask_resized = np.expand_dims(mask_resized, -1) / 255
+    img_resized = img_resized * (1-mask_resized) + img * mask_resized
+    return img_resized
+
+
+def predict_masks_with_sam(
+        img: np.ndarray,
+        point_coords: List[List[float]],
+        point_labels: List[int],
+        model_type: str,
+        ckpt_p: str,
+        device="cuda"
+):
+    point_coords = np.array(point_coords)
+    point_labels = np.array(point_labels)
+    # Load SAM model
+    sam = sam_model_registry[model_type](checkpoint=ckpt_p)
+    sam.to(device=device)
+    predictor = SamPredictor(sam)
+
+    predictor.set_image(img) # Set input image for prediction
+    masks, scores, logits = predictor.predict(
+        point_coords=point_coords,
+        point_labels=point_labels,
+        multimask_output=True,
+    )
+    # Return all masks and metadata
+    return masks, scores, logits
+
+def setup_args(parser):
+    parser.add_argument(
+        "--input_img", type=str, required=True,
+        help="Path to a single input img",
+    )
+    parser.add_argument(
+        "--coords_type", type=str, required=True,
+        default="key_in", choices=["click", "key_in"], 
+        help="The way to select coords",
+    )
+    parser.add_argument(
+        "--point_coords", type=float, nargs='+', required=True,
+        help="The coordinate of the point prompt, [coord_W coord_H].",
+    )
+    parser.add_argument(
+        "--point_labels", type=int, nargs='+', required=True,
+        help="The labels of the point prompt, 1 or 0.",
+    )
+    parser.add_argument(
+        "--text_prompt", type=str, required=True,
+        help="Text prompt",
+    )
+    parser.add_argument(
+        "--dilate_kernel_size", type=int, default=None,
+        help="Dilate kernel size. Default: None",
+    )
+    parser.add_argument(
+        "--output_dir", type=str, required=True,
+        help="Output path to the directory with results.",
+    )
+    parser.add_argument(
+        "--sam_model_type", type=str,
+        default="vit_h", choices=['vit_h', 'vit_l', 'vit_b', 'vit_t'],
+        help="The type of sam model to load. Default: 'vit_h"
+    )
+    parser.add_argument(
+        "--sam_ckpt", type=str, required=True,
+        help="The path to the SAM checkpoint to use for mask generation.",
+    )
+    parser.add_argument(
+        "--seed", type=int,
+        help="Specify seed for reproducibility.",
+    )
+    parser.add_argument(
+        "--deterministic", action="store_true",
+        help="Use deterministic algorithms for reproducibility.",
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    setup_args(parser)
+    args = parser.parse_args(sys.argv[1:])
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cpu"
+    
+    # Get coordinates either by clicking or key input
+    if args.coords_type == "click":
+        latest_coords = get_clicked_point(args.input_img)
+    elif args.coords_type == "key_in":
+        latest_coords = args.point_coords
+    img = load_img_to_array(args.input_img) # Load image
+
+    masks, _, _ = predict_masks_with_sam(
+        img,
+        [latest_coords],
+        args.point_labels,
+        model_type=args.sam_model_type,
+        ckpt_p=args.sam_ckpt,
+        device=device,
+    )
+    masks = masks.astype(np.uint8) * 255 # Convert masks to binary image
+
+    # dilate mask to avoid unmasked edge effect
+    if args.dilate_kernel_size is not None:
+        masks = [dilate_mask(mask, args.dilate_kernel_size) for mask in masks]
+
+    # visualize the segmentation results
+    img_stem = Path(args.input_img).stem
+    out_dir = Path(args.output_dir) / img_stem
+    out_dir.mkdir(parents=True, exist_ok=True)
+    for idx, mask in enumerate(masks):
+        # path to the results
+        mask_p = out_dir / f"mask_{idx}.png"
+        img_points_p = out_dir / f"with_points.png"
+        img_mask_p = out_dir / f"with_{Path(mask_p).name}"
+
+        # save the mask
+        save_array_to_img(mask, mask_p)
+
+        # save the pointed and masked image
+        dpi = plt.rcParams['figure.dpi']
+        height, width = img.shape[:2]
+        plt.figure(figsize=(width/dpi/0.77, height/dpi/0.77))
+        plt.imshow(img)
+        plt.axis('off')
+        show_points(plt.gca(), [latest_coords], args.point_labels, size=(width*0.04)**2)
+        plt.savefig(img_points_p, bbox_inches='tight', pad_inches=0)
+        show_mask(plt.gca(), mask, random_color=False)
+        plt.savefig(img_mask_p, bbox_inches='tight', pad_inches=0)
+        plt.close()
+
+    # fill the masked image
+    for idx, mask in enumerate(masks):
+        if args.seed is not None:
+            torch.manual_seed(args.seed)
+        mask_p = out_dir / f"mask_{idx}.png"
+        img_replaced_p = out_dir / f"replaced_with_{Path(mask_p).name}"
+        img_replaced = replace_img_with_sd(img, mask, args.text_prompt, device=device)
+        save_array_to_img(img_replaced, img_replaced_p)  
